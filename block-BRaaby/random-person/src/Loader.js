288306f1b0f13143f6b99abad5f4ba8d9cc04c6f
Vineet <vineet98love@gmail.com>
2022-02-21 18:44:17 +0530

    update

---
+++ block-BRaaby/random-person/src/Loader.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './loader.css'
+
+const Loader = () => {
+    return (
+        <div className="bouncing-loader">
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+
+    )
+}
+
+export default Loader
